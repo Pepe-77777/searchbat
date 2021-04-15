fab1ab7cca76dc35d10ab860d847f58971ca6e48
Pepê <81209437+Pepe-77777@users.noreply.github.com>
2021-04-15 16:56:25 -0300

    Add files via upload

--- App.bat
+++ App.bat
@@ -1,16 +1,36 @@
 @echo off
 cls
+:1
+title Search Bat - Created by Pepê!
 echo APP.bat
 echo.
-echo -----------------------
+echo ----------------------------
 echo 1- Pesquisar no YOUTUBE
 echo 2- Pesquisar no GOOGLE
 echo 3- Fechar
-echo -----------------------
+echo 4- Abrir a pagina do GITHUB
+echo 5- Desinstalar
+echo ----------------------------
+echo Desenvolvido pelo Pepê!
 set /p opcao=Escolha a opção: 
 if %opcao%==1 goto YOUTUBE
 if %opcao%==2 goto GOOGLE
 if %opcao%==3 goto FECHAR
+if %opcao%==4 goto GITHUB
+if %opcao%==5 goto DESINSTALAR
+
+:DESINSTALAR
+clear
+set /p desisn=Você tem certeza que quer desisntalar? (S ou N): 
+if %desisn%==N goto 1
+if %desisn%==S goto 2
+
+:2
+start Desinstalar.bat
+
+
+:GITHUB
+start www.github.com/Pepe-77777/searchbat
 
 :YOUTUBE
 cd Assets
