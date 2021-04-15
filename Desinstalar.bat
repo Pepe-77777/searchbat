127f3df9536f626569c29b6183ec4c17555141ed
Pepê <81209437+Pepe-77777@users.noreply.github.com>
2021-04-15 17:01:08 -0300

    Add files via upload

--- Desinstalar.bat
+++ Desinstalar.bat
@@ -4,3 +4,4 @@ del App.bat
 echo (requer) Digite S ou Y na proxima, se não a desinstalação não vai ser concluida com sucesso!
 rd /s Assets
 echo Para finalizar, delete esse arquivo em C:\Program Files\searchbat
+pause > nul
