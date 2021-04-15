f35cbf24ebd47ca5551ccc32ee9df35ec35b4c66
Pepê <81209437+Pepe-77777@users.noreply.github.com>
2021-04-15 16:47:44 -0300

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -5,7 +5,8 @@ pause > nul
 cd C:\Program Files
 git clone https://github.com/Pepe-77777/searchbat
 echo (opcional) escreva S ou Y agora, para deletar os arquivos do git (.git)
+cd SearchBat
 rd /s .git
 del README.md > nul
-cd SearchBat
+del Setup.bat > nul
 start App.bat
